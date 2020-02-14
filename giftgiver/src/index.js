6777d0e0bf34a7a5298eee769e3b690c74f9011d
Fred M <farzad.moghbel@gmail.com>
2020-02-14 01:28:03 -0800

    add a gift to rendered list-onClick-done

--- giftgiver/src/index.js
+++ giftgiver/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
-render(<Ap />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
